58e1052c9fac6d74a3c26f12295208a545ac14d9
mun-jihye <m84422583@gmail.com>
2024-05-24 16:20:12 +0900

    next project init setting

---
+++ playground/pages/_document.tsx
@@ -0,0 +1,12 @@
+import { Html, Head } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <div>hi</div>
+      </body>
+    </Html>
+  );
+}
